180da93b0b6054e03fbe8179a4ffb8417bf087c9
Kendall Werts <kmwerts@gmail.com>
2025-01-27 15:15:52 -0600

    fix push script

--- push_image.sh
+++ push_image.sh
@@ -3,9 +3,9 @@
 set -ex
 
 aws ecr get-login-password --region us-east-2 --profile kendall | docker login --username AWS --password-stdin 340531845404.dkr.ecr.us-east-2.amazonaws.com
-docker build . -t spc --provenance=false
+docker buildx build --platform linux/amd64 -t spc_linux . --provenance=false
 export tag=$(date +%s)
-docker tag spc 340531845404.dkr.ecr.us-east-2.amazonaws.com/spc:$tag
-docker tag spc 340531845404.dkr.ecr.us-east-2.amazonaws.com/spc:latest
+docker tag spc_linux 340531845404.dkr.ecr.us-east-2.amazonaws.com/spc:$tag
+docker tag spc_linux 340531845404.dkr.ecr.us-east-2.amazonaws.com/spc:latest
 docker push 340531845404.dkr.ecr.us-east-2.amazonaws.com/spc:$tag
 docker push 340531845404.dkr.ecr.us-east-2.amazonaws.com/spc:latest
--- src/spc_loc/__main__.py
+++ src/spc_loc/__main__.py
@@ -5,8 +5,14 @@ from spc_loc.spc import (
     send_notification,
 )
 
-if __name__ == "__main__":
+
+def lambda_handler(event, context):
     get_spc_day_1()
     gdf = load_day_1()
     label = check_loc_in_outlook(gdf)
     send_notification(label)
+    return {"statusCode": 200, "body": "Success!"}
+
+
+if __name__ == "__main__":
+    lambda_handler(None, None)
